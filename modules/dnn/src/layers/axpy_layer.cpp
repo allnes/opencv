0d3fd28e759fc157f72625fc71e23b363f3b1b15
Alexander Nesterov <nesoldr@mail.ru>
2019-03-28 13:08:19 -0100

    Update layer 2

--- modules/dnn/src/layers/axpy_layer.cpp
+++ modules/dnn/src/layers/axpy_layer.cpp
@@ -43,25 +43,24 @@ public:
         outputs_arr.getMatVector(outputs);
 
         Mat alpha, x_array, y_array, result;
-        alpha = inputs[0].reshape(1, inputs[0] .size[1]);
-        transpose(inputs[0].reshape(1, inputs[0] .size[1]), alpha);
+        transpose(inputs[0] .reshape(1, inputs[0] .size[1]), alpha);
         transpose(inputs[1] .reshape(1, inputs[1] .size[1]), x_array);
         transpose(inputs[2] .reshape(1, inputs[2] .size[1]), y_array);
         transpose(outputs[0].reshape(1, outputs[0].size[1]), result);
 
-        std::cout << alpha.size << '\n';
-        std::cout << x_array.size << '\n';
-        std::cout << y_array.size << '\n';
-        std::cout << result.size << '\n';
+        // std::cout << alpha.size << '\n';
+        // std::cout << x_array.size << '\n';
+        // std::cout << y_array.size << '\n';
+        // std::cout << result.size << '\n';
 
         for (size_t i = 0; i < x_array.size[1]; i++)
         {
-            cv::multiply(x_array.row(i), alpha.row(i), result.row(i));
+            cv::multiply(x_array.col(i), alpha.at<float>(0, i), result.col(i));
         }
         cv::add(result, y_array, result);
 
-        outputs.clear();
-        outputs.push_back(inputs[2].clone());
+        // outputs.clear();
+        // outputs.push_back(inputs[2].clone());
         // std::cout << outputs[0].size << '\n';
         // std::cout << outputs.size() << '\n';
     }
